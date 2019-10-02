05150fb865cdbed30bec2252ae8f6cc05a9bb2c3
Het Shah <htshah60@gmail.com>
2019-10-03 00:49:35 +0530

    build: almost final changes for rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,34 @@
 import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
+// import minify from 'rollup-plugin-babel-minify';
+
+const fileName = `websocket-pubsub.js`;
 
 export default {
   input: 'src/index.js',
   output: {
-    file: './dist/websocket-pubsub.js',
-    format: 'umd',
+    file: `./dist/${fileName}`,
+    format: 'iife',
     name: 'WebsocketPubSub'
   },
   plugins: [
+    replace({
+      PRODUCTION: process.env.NODE_ENV === 'production'
+    }),
     babel({
-      presets: ['@babel/preset-env'],
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              browsers: '> 0.14%, not ie <= 9'
+            },
+            loose: true
+          }
+        ]
+      ],
       exclude: 'node_modules/**'
     })
+    // minify()
   ]
 };
