7810acce69d51f2961a797c179e02cdba08c57cb
Het Shah <htshah60@gmail.com>
2019-10-13 18:18:06 +0530

    feat: added chat demo

---
+++ demos/chat/js/main.js
@@ -0,0 +1,39 @@
+$(function() {
+  var ws = new WebsocketPubSub("ws://websocket-pubsub-server.herokuapp.com", {
+    reconnect: false,
+    timeout: 10000
+  });
+
+  ws.subscribe("message", function(msg) {
+    addMsgToChatBox(JSON.parse(msg));
+  });
+
+  var $chatBoxBody = $(".chat-box__body");
+
+  $(".chat-box__input").keypress(function(e) {
+    var $this = $(this);
+    var msg = $this.val();
+    var parsedMsg = {
+      author: authorName,
+      message: msg
+    };
+    if (e.which == 13) {
+      ws.emit("broadcast", parsedMsg);
+      parsedMsg.author = "You";
+      addMsgToChatBox(parsedMsg, true);
+      $this.val("");
+    }
+  });
+
+  function addMsgToChatBox(msg, self) {
+    var div = $('<div class="chat-box__msg">');
+    var author = "<span>" + msg.author + "</span>";
+    div.addClass("chat-box__" + (!self ? "other" : "me"));
+
+    div.html("<div>" + author + msg.message + "</div>");
+
+    $chatBoxBody.append(div);
+
+    $chatBoxBody[0].scrollTop = $chatBoxBody[0].scrollHeight;
+  }
+});
---
+++ demos/chat/js/websocket-pubsub.js
@@ -0,0 +1,202 @@
+var WebsocketPubSub = (function () {
+  'use strict';
+
+  function log() {
+    // eslint-disable-next-line no-unused-expressions,no-undef,no-console
+     console.log.apply(null, arguments);
+  }
+  /**
+   * Overwrites all the values in 'a' by values
+   * in 'b' and returns a new object containing
+   * new values.
+   *
+   * @param {Object} a Object in which overwrites will happen
+   * @param {Object} b Object from which new values will be taken
+   */
+
+  function updateDefaults(a, b) {
+    var temp = {};
+    Object.keys(a).forEach(function (key) {
+      // eslint-disable-next-line no-prototype-builtins
+      temp[key] = b.hasOwnProperty(key) ? b[key] : a[key];
+    });
+    return temp;
+  }
+  function split(str, delimiter) {
+    var pos = str.indexOf(delimiter);
+    return [str.substr(0, pos), str.substr(pos + 1)];
+  }
+
+  var WebsocketPubSub = function WebsocketPubSub(url, userOptions) {
+    log('🚀 Creating a websocket...');
+    this.ws = this._createObj(url);
+    log('🧰 Configuring...'); // Default options
+
+    var defaultOptions = {
+      reconnect: true,
+      attempts: 5,
+      timeout: 3000,
+      buffer: true,
+      url: url
+    }; // Overwrite default options
+
+    this._options = updateDefaults(defaultOptions, userOptions || {});
+    log('✔ Configured:', this._options);
+
+    this._init();
+  };
+
+  WebsocketPubSub.prototype = {
+    _uid: -1,
+    // Stores last UID generated
+    _delimiter: ' ',
+    // Delimiter for channel
+    _sub: {},
+    // Stores list of subscriptions for each channels registered.
+    isOpen: false,
+    // Tells whether the websocket is connected or not.
+    _buffer: [],
+    _lastPing: false,
+    _stillConnected: false,
+    _createObj: function _createObj(url) {
+      // Use MozWebSocket if browser is Firefox
+      var Socket = 'MozWebSocket' in window ? window.MozWebSocket : window.WebSocket; // Create a new Socket object
+
+      return new Socket(url);
+    },
+    _init: function _init() {
+      var _this = this;
+
+      this.isOpen = false;
+
+      this.ws.onopen = function () {
+        log('Connection opened');
+        _this.isOpen = true; // Bind events
+
+        _this._onMessage();
+
+        _this.ws.onclose = _this._onClose;
+
+        _this._onError(); // Send buffered messages.
+
+
+        _this._emitBuffered(); // Continuously check if server is still
+        // connected or not.
+        //! __ is a reserved channel name.
+
+
+        _this._key = _this.subscribe('__', function (data) {
+          if (data === 'ping' || data === 'pong') {
+            _this._stillConnected = true;
+          }
+        });
+        _this._pingLoop = setInterval(function () {
+          if (_this._lastPing !== false && !_this._stillConnected) {
+            _this._onClose(); // Close connection
+
+
+            return;
+          }
+
+          _this._lastPing = true;
+          _this._stillConnected = false;
+
+          _this.emit('__', 'ping');
+        }, _this._options.timeout);
+      }; // Autoclose after a timeout if no connection can be made
+
+
+      setTimeout(function () {
+        if (_this.ws !== null && (_this.ws.readyState === 0 || _this.ws.readyState === 3)) {
+          log("Couldn't connect to target. Closing connection...");
+
+          _this._onClose();
+        }
+      }, this._options.timeout);
+    },
+    _onMessage: function _onMessage() {
+      var _this2 = this;
+
+      this.ws.onmessage = function (event) {
+        log("Received: " + event.data); // Separate channel and payload;
+
+        var parsedData = split(event.data, _this2._delimiter);
+        var channel = parsedData[0];
+        var payload = parsedData[1]; // Invoke all the callbacks for given
+        // channel.
+
+        var cbs = _this2._sub[channel];
+        Object.keys(cbs).map(function (k) {
+          return cbs[k](payload, event);
+        });
+      };
+    },
+    _onClose: function _onClose() {
+      log('❌ Closing');
+      clearInterval(this._pingLoop);
+
+      if (this._options.reconnect && this._options.attempts > 0) {
+        log("Reconnecting..");
+        this._options.attempts -= 1;
+        this.unsubscribe(this._key);
+        this.ws = this._createObj(this._options.url);
+
+        this._init();
+      } else {
+        log('✔ Closed');
+      }
+    },
+    _onError: function _onError() {
+      this.ws.onerror = function () {
+        log('Some error occured');
+      };
+    },
+    emit: function emit(channel, payload) {
+      log("Emitting: " + payload);
+      var parsedPayload = JSON.stringify(payload);
+      var msg = channel + " " + parsedPayload;
+
+      if (this.isOpen) {
+        this.ws.send(msg);
+      } else if (this._options.buffer) {
+        this._buffer.push(msg);
+      }
+    },
+    _emitBuffered: function _emitBuffered() {
+      var _this3 = this;
+
+      if (this.isOpen && this._buffer.length > 0) {
+        this._buffer.forEach(function (msg) {
+          log("Emitting buffered: " + msg);
+
+          _this3.ws.send(msg);
+        });
+
+        this._buffer = [];
+      }
+    },
+    subscribe: function subscribe(channel, cb) {
+      log("Subscribing to " + channel);
+      var sub = this._sub;
+      this._uid += 1;
+      sub[channel] = sub[channel] || {};
+      sub[channel][this._uid] = cb;
+      return channel + " " + this._uid;
+    },
+    unsubscribe: function unsubscribe(key) {
+      if (key === undefined) {
+        return;
+      }
+
+      log("Unsubscribing from " + key);
+      var parsedKey = split(key, ' ');
+      var channel = parsedKey[0];
+      var uid = parsedKey[1]; // Remove cb from the channel's list.
+
+      delete this._sub[channel][uid];
+    }
+  };
+
+  return WebsocketPubSub;
+
+}());
