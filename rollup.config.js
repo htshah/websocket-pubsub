11013c2a55c2b81004bd24b1a02667a8e9905c22
Het Shah <htshah60@gmail.com>
2021-05-17 16:39:03 +0530

    build: updated rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,52 @@
-import babel from 'rollup-plugin-babel';
-import replace from 'rollup-plugin-replace';
-// import minify from 'rollup-plugin-babel-minify';
+import { babel } from '@rollup/plugin-babel';
+import replace from '@rollup/plugin-replace';
+import { terser } from 'rollup-plugin-terser';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
-const fileName = `websocket-pubsub.js`;
+const fileName = `websocket-pubsub`;
+const inputFile = 'src/index.js';
+const iifeName = 'WebsocketPubSub';
+const isProduction = process.env.NODE_ENV === 'production';
+
+const rollupPlugins = [
+  replace({
+    values: {
+      PRODUCTION: isProduction,
+    },
+    preventAssignment: true,
+  }),
+  nodeResolve(),
+  babel({
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          loose: true,
+          modules: false,
+        },
+      ],
+    ],
+    exclude: 'node_modules/**',
+    babelHelpers: 'bundled',
+    plugins: [['@babel/plugin-proposal-class-properties', { loose: true }]],
+  }),
+];
 
 export default {
-  input: 'src/index.js',
-  output: {
-    file: `./dist/${fileName}`,
-    format: 'iife',
-    name: 'WebsocketPubSub'
-  },
-  plugins: [
-    replace({
-      PRODUCTION: process.env.NODE_ENV === 'production'
-    }),
-    babel({
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            targets: {
-              browsers: '> 0.14%, not ie <= 9'
-            },
-            loose: true
-          }
-        ]
-      ],
-      exclude: 'node_modules/**'
-    })
-    // minify()
-  ]
+  input: inputFile,
+  plugins: rollupPlugins,
+  output: [
+    {
+      file: `./dist/${fileName}.js`,
+      format: 'iife',
+      name: iifeName,
+      sourcemap: true,
+    },
+    {
+      file: `./dist/${fileName}.min.js`,
+      format: 'iife',
+      name: iifeName,
+      plugins: [terser()],
+    },
+  ],
 };
